8eb473d3cc63a3a05ed95bcbeabf3ead8d24e787
Venkata Prithvi Krishna Palle <vp430@snu.edu.in>
2019-04-26 18:04:00 +0530

    Updated Reg

--- src/Reg.js
+++ src/Reg.js
@@ -1,178 +1,41 @@
 // Registration Page
 import React,{Component} from 'react';
-import Logo from './diet.svg';
+import logo from './diet.svg';
 import './App.css';
 import FormFields from './formfields';
 import {Link} from 'react-router-dom';
 
 
-
-
-
-
-
-class Signup extends Component{
-
-    state = {
-
-        formData:{
-            name:{
-                element:'input',
-                value:'',
-                config:{
-                    name:'name_input',
-                    type:'text',
-                    placeholder:'Name '
-                },
-                validation:{
-                    required:true,
-                    minLen:5
-                },
-                valid:false,
-                touched:false,
-                validationMessage:''
-            },
-
-            username :{
-                element:'input',
-                value:'',
-                config:{
-                    name:'username_input',
-                    type:'text',
-                    placeholder:'Username'
-                },
-                validation:{
-                    required:true
-                },
-                valid:false,
-                touched:false,
-                validationMessage:''
-                // have to check for uniqueness 
-            },
-            emailid:{
-                element:'input',
-                value:'',
-                config:{
-                    name:'emailid_input',
-                    type:'email',
-                    placeholder:'Email id'
-                },
-                validation:{
-                    required:true
-                },
-                valid:false,
-                touched:false,
-                validationMessage:''
-            },
-            contactnumber:{
-                element:'input',
-                value:'',
-                config:{
-                    name:'contact_input',
-                    type:'number',
-                    placeholder:'Contact Number'
-                },
-                validation:{
-                    required:true
-                },
-                valid:false,
-                touched:false,
-                validationMessage:''
-            },
-            
-            password:{
-                element:'input',
-                value:'',
-                config:{
-                    name:'password',
-                    type:'password',
-                    placeholder:'Password'
-                },
-                validation:{
-                    required:true
-                },
-                valid:false,
-                touched:false,
-                validationMessage:''
-            }
-
-
-        
-    }}
-
-    updateForm = (newState) => {
-        this.setState({
-            formData:newState
-        })
+class Reg extends Component
+{
+    constructor(props)
+    {
+        super(props);
     }
-
-
-submitForm = (event) => {
-        event.preventDefault();
-        let dataToSubmit = {};
-        let formIsValid = true;
-
-        for(let key in this.state.formData){
-            dataToSubmit[key] = this.state.formData[key].value;
-        }
-
-        for(let key in this.state.formData){
-            formIsValid = this.state.formData[key].valid && formIsValid;
-        }
-
-        if(formIsValid){
-         //   firebaseDB.ref('users').push(dataToSubmit)
-        //  .then(()=>{
-               console.log('new user added') 
-          //  }).catch( e =>{
-     //           console.log(e)
-   //         })
-        }
-    
-    }
-
-
- 
-
     render(){
         return(
-
-
-<div className="App">
-        <div >
-        <Logo/>
-
-<div className = "">
-<form onSubmit={this.submitForm}>
-
-
-<FormFields
-        formData={this.state.formData}
-        onblur={(newState) => this.updateForm(newState)}
-        change={(newState) => this.updateForm(newState)}
-    />
-
-    <button type="submit">submit</button>
-
-</form>
-</div>
-
-        </div>
-        </div>
-        )
+            <div className="App">
+                <div className="loginbox" >
+                <img src={logo} className="avatar" alt="logo"></img>
+                <h1 style={{}}>SNU TREATS</h1>
+                <form>
+                  <input placeholder="Username" onChange={this.handleUsername} required></input>
+                  <br></br>
+                  <input placeholder="SNU Mail" type="email" pattern=".+@snu.edu.in" onChange={this.handleEmail} required></input>
+                  <br></br>
+                  <input placeholder="Phone Number" type="tel" id="phone" name="phone"
+                        pattern="[7-9]{1}[0-9]{9}" required></input>
+                  <br></br>
+                  <input placeholder="Password" type = "password" onChange={this.handlePass} required></input>
+                  <br></br>
+                  <button className="loginB" onClick={this.authenticate} >Register</button>
+                </form>
+              </div>                
+            </div>
+        );
     }
-
 }
 
-export default Signup;
 
-function Reg()
-{
-    return(
-        <div>
-            
-        </div>
-    );
-}
 
-//export default Reg;
+export default Reg;
